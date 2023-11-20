bd6ff0fc60ee3b260a3958777e89ec15869b6f1f
ShumnaIryna <87974828+ShumnaIryna@users.noreply.github.com>
2023-11-20 16:28:31 +0200

    added dynamic video player

--- src/js/main.js
+++ src/js/main.js
@@ -45,8 +45,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
   feedSlider.init();
 
-  const player = new VideoPlayer(".showup .play", ".overlay");
-  player.init();
+  new VideoPlayer(".showup .play", ".overlay").init();
+  // on the page Modules
+  new VideoPlayer(".module__video-item .play", ".overlay").init();
 
   new Difference(".officerold", ".officernew", ".officer__card-item").init();
   new Form(".form").init();
